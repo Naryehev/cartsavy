ec8aec16dc27bf46a76c80660f6ce311c28eb003
Xaniel <daniel.naryshev@gmail.com>
2017-07-06 22:20:27 -0700

    updates

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,11 @@ $(document).ready(function() {
       });
   });
 });
+function h(e) {
+  $(e).css({'height':'auto','overflow-y':'hidden'}).height(e.scrollHeight);
+}
+$('textarea').each(function () {
+  h(this);
+}).on('input', function () {
+  h(this);
+});
